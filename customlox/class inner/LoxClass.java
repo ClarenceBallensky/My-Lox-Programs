a7b764a9d2f495176c3b229783f2cf8c99bf7261
Clarence Ballensky <alballensky@cpp.edu>
2026-03-07 19:24:02 -0800

    CH13, Q2: replaced Lox's super behavior with BETA's inner semantics

---
+++ customlox/class inner/LoxClass.java
@@ -0,0 +1,109 @@
+//> Classes lox-class
+package com.craftinginterpreters.lox;
+
+import java.util.List;
+import java.util.Map;
+
+/* Classes lox-class < Classes lox-class-callable
+class LoxClass {
+*/
+//> lox-class-callable
+class LoxClass implements LoxCallable {
+//< lox-class-callable
+  final String name;
+//> Inheritance lox-class-superclass-field
+  final LoxClass superclass;
+//< Inheritance lox-class-superclass-field
+/* Classes lox-class < Classes lox-class-methods
+
+  LoxClass(String name) {
+    this.name = name;
+  }
+*/
+//> lox-class-methods
+  private final Map<String, LoxFunction> methods;
+
+/* Classes lox-class-methods < Inheritance lox-class-constructor
+  LoxClass(String name, Map<String, LoxFunction> methods) {
+*/
+//> Inheritance lox-class-constructor
+  LoxClass(String name, LoxClass superclass,
+           Map<String, LoxFunction> methods) {
+    this.superclass = superclass;
+//< Inheritance lox-class-constructor
+    this.name = name;
+    this.methods = methods;
+  }
+//< lox-class-methods
+//> lox-class-find-method
+  /*LoxFunction findMethod(String name) {
+    if (methods.containsKey(name)) {
+      return methods.get(name);
+    }
+
+//> Inheritance find-method-recurse-superclass
+    if (superclass != null) {
+      return superclass.findMethod(name);
+    }
+
+//< Inheritance find-method-recurse-superclass
+    return null;
+  }*/
+
+  //CH13, Q2 START
+    LoxFunction findMethod(LoxInstance instance, String name) {
+      LoxFunction method = null;
+      LoxFunction inner = null;
+      LoxClass klass = this;
+      while (klass != null) {
+        if (klass.methods.containsKey(name)) {
+          inner = method;
+          method = klass.methods.get(name);
+        }
+
+        klass = klass.superclass;
+      }
+
+      if (method != null) {
+        return method.bind(instance, inner);
+      }
+
+      return null;
+    }
+  //CH13, Q2 END
+
+//< lox-class-find-method
+
+  @Override
+  public String toString() {
+    return name;
+  }
+//> lox-class-call-arity
+  @Override
+  public Object call(Interpreter interpreter,
+                     List<Object> arguments) {
+    LoxInstance instance = new LoxInstance(this);
+//> lox-class-call-initializer
+    LoxFunction initializer = findMethod(instance, "init");
+    if (initializer != null) {
+      initializer.call(interpreter, arguments);
+    }
+
+//< lox-class-call-initializer
+    return instance;
+  }
+
+  @Override
+  public int arity() {
+/* Classes lox-class-call-arity < Classes lox-initializer-arity
+    return 0;
+*/
+//> lox-initializer-arity
+    LoxInstance instance = new LoxInstance(this);
+    LoxFunction initializer = findMethod(instance, "init");
+    if (initializer == null) return 0;
+    return initializer.arity();
+//< lox-initializer-arity
+  }
+//< lox-class-call-arity
+}
